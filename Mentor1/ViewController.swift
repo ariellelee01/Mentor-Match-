101fd2f5d3f278343edf4c7e8f61be0223bf8856
Profile <al416494@imcm9.lib.albany.edu>
2017-03-01 17:05:21 -0500

    Initial Commit

---
+++ Mentor1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mentor1
+//
+//  Created by Lee, Arielle A on 3/1/17.
+//  Copyright © 2017 Lee, Arielle A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
